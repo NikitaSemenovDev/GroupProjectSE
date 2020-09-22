349bbe89eeb5811df775b7662842b895024b6eaa
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2020-09-22 13:01:34 +0500

    Adjusted application environment

---
+++ GroupProjectSE/GroupProjectSE/Services/DependencyRegistration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GroupProjectSE.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GroupProjectSE.Services
+{
+    public static class DependencyRegistration
+    {
+        public static void AddDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<ILogger, Log4NetLogger>();
+        }
+    }
+}
---
+++ GroupProjectSE/GroupProjectSE/Startup.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using GroupProjectSE.Controllers;
+using GroupProjectSE.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+
+namespace GroupProjectSE
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDependencies();
+
+            services.AddSwaggerGen(setup =>
+            {
+                setup.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Version = "v1",
+                    Title = "GroupProject API",
+                    Description = "API for WEB and mobile applications"
+                });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                setup.IncludeXmlComments(xmlPath);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            string configFilePath = Path.Combine(env.ContentRootPath, "log4net.config");
+            loggerFactory.AddLog4Net(configFilePath);
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(s =>
+            {
+                s.SwaggerEndpoint("/swagger/v1/swagger.json", "GroupProject API V1");
+            });
+        }
+    }
+}
