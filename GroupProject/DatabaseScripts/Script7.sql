2bdb4c055e07917d31eda1c5b63e45008ce47866
SemenovHSE <31741736+SemenovHSE@users.noreply.github.com>
2021-03-15 16:43:17 +0500

    Updated ImageProcessingResult fields

---
+++ GroupProject/DatabaseScripts/Script7.sql
@@ -0,0 +1,14 @@
+﻿USE GroupProject
+
+GO
+
+TRUNCATE TABLE ImageProcessingResult;
+
+ALTER TABLE ImageProcessingResult
+ALTER COLUMN RegionsPredictions NVARCHAR(MAX) NOT NULL;
+
+ALTER TABLE ImageProcessingResult
+ALTER COLUMN DiseasesNames NVARCHAR(1000) NOT NULL;
+
+ALTER TABLE ImageProcessingResult
+ALTER COLUMN ImagePredictions NVARCHAR(MAX) NOT NULL;
